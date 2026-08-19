2d98b48ccc549d5b523cd963553c646231991c24
Panda <735591750@QQ.COM>
2026-08-19 11:43:46 +0800

    feat: add package initializer

---
+++ src/schematic_mcp/__init__.py
@@ -0,0 +1,6 @@
+"""schematic-mcp package."""
+
+from .models import Component, Net, Pin, Point, Schematic, Wire
+
+__all__ = ["Component", "Net", "Pin", "Point", "Schematic", "Wire"]
+__version__ = "0.1.0"
